b86ff60277f56b60354c4178fa0a233d8e1c005f
Qiang Yiting <qiangyt@wxcount.com>
2020-01-31 18:48:22 +0800

    update right-menu in navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,16 +11,16 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
+          <router-link to="/example">
             <el-dropdown-item>
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
+          <a target="_blank" href="https://github.com/qiangyt/springboot-example-web">
+            <el-dropdown-item>Project GITHUB</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+            <el-dropdown-item>Vue-admin-template GITHUB</el-dropdown-item>
           </a>
           <el-dropdown-item divided @click.native="logout">
             <span style="display:block;">Log Out</span>
